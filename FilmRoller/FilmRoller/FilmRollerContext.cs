5bd8b2b26ac7f7e338c5eecb9b88b8cc4329fdc7
aszhitarev <aszhitarev@gmail.com>
2017-04-23 18:20:07 +0300

    Контект для БД

---
+++ FilmRoller/FilmRoller/FilmRollerContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+
+namespace FilmRoller
+{
+    class FilmRollerContext
+    {
+        class FilmRollerContext : DbContext
+        {
+            public FilmRollerContext()
+                : base("FilmRoller")
+            { }
+
+            public DbSet<Film> Films { get; set; }
+            public DbSet<Owner> Owners { get; set; }
+            public DbSet<Review> Reviews { get; set; }
+        }
+    }
+}
