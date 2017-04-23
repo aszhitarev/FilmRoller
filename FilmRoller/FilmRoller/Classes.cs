e9bdecacde58d38447fedb7bc7982955f1bf2371
aszhitarev <aszhitarev@gmail.com>
2017-04-23 17:14:42 +0300

    Написан класс Film

---
+++ FilmRoller/FilmRoller/Classes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilmRoller
+{
+    public class Film
+    {
+        int FilmId { get; set; }
+        int FilmName { get; set; }
+        int FilmOwner { get; set; }
+    }
+}
